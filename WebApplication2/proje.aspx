50e394a84c698c5b5c2eb89fcd4e70cc1ce5d025
Cem Şahin <cmshn02@gmail.com>
2022-05-29 11:46:48 +0300

    Add project files.

---
+++ WebApplication2/proje.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="proje.aspx.cs" Inherits="WebApplication2.proje" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:TextBox ID="txtzaman" TextMode="Date" runat="server"></asp:TextBox> <br /> <br />
+          
+            <asp:Button ID="btnhesapla" runat="server" Text="Hesapla" OnClick="btnhesapla_Click" /> <br /> <br />
+
+            <asp:Label ID="lblmesaj" runat="server" Text=""></asp:Label> <hr />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/proje.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class proje : System.Web.UI.Page
+    {
+
+       
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnhesapla_Click(object sender, EventArgs e)
+        {
+
+
+
+            DateTime eskıgun =Convert.ToDateTime(txtzaman.Text);
+            DateTime bugun =Convert.ToDateTime(DateTime.Today);
+
+
+            TimeSpan ts = bugun - eskıgun;
+          
+
+
+            lblmesaj.Text = "Geçen Süre = " + ts.Days.ToString() + "  Gün " ;
+
+        }
+    }
+}
